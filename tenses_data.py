ff7efbf0dc2bae44d291e2f48c181723895d8d83
williamdwinnell <93562392+williamdwinnell@users.noreply.github.com>
2023-02-21 17:39:02 -0500

    Added tense data structure with empty data

---
+++ tenses_data.py
@@ -0,0 +1,22 @@
+tenses = {
+    'Present': "",
+    'Preterite': "",
+    'Imperfect': "",
+    'Future': "",
+    'Conditional': "",
+    'Present perfect': "",
+    'Pluperfect': "",
+    'Future perfect': "",
+    'Conditional perfect': "",
+    'Present subjunctive': "",
+    'Imperfect subjunctive': "",
+    'Future subjunctive': "",
+    'Present perfect subjunctive': "",
+    'Pluperfect subjunctive': "",
+    'Future perfect subjunctive': "",
+    'Imperative mood': "",
+    'Infinitive mood': "",
+    'Gerund mood': "",
+}
+
+print(tenses['Present'])
